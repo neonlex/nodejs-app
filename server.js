ca68de76178685dac01dc80fd10b7a5fb08daabb
Daniel Hüsch <daniel.huesch@gmail.com>
2011-05-20 21:23:03 +0200

    Add mysql example.

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var http = require('http');
+var scalarium = require('scalarium'),
+    http = require('http'),
+    client = new require('mysql').Client();
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  client.user = scalarium.db.username;
+  client.password = scalarium.db.password;
+  client.host = scalarium.db.host;
+  client.connect();
+
+  // do interesting stuff with the mysql client
+
+  client.end();
   res.end();
 }).listen(80);

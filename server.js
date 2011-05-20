0ce47f0d1d15733011d1fe0d80cb3542d648e981
Daniel Hüsch <daniel.huesch@gmail.com>
2011-05-20 21:28:16 +0200

    Add memcache example.

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-var http = require('http');
+var scalarium = require('scalarium'),
+    http = require('http'),
+    client = new (require('memcache').Client)(scalarium.memcached.port, scalarium.memcached.host);
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  client.connect();
+
+  // do interesting stuff with the memcache client
+
+  client.close();
   res.end();
 }).listen(80);

9da342bf52cfad11e8ecc683e0287130584d1ab2
Daniel Hüsch <daniel.huesch@gmail.com>
2011-06-11 13:51:00 +0200

    Add greeting.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end();
-}).listen(80);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Hello, nodecamp</h1>');
+}).listen(8080);

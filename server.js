a793930f46fdee7d3fa01de78f129e8aa0c62d7b
Daniel Hüsch <daniel.huesch@gmail.com>
2011-11-14 12:24:10 +0100

    Show node.js version as greeting.

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>Hello, nodecamp</h1>');
+  res.end('<h1>This is node.js ' + process.version + '</h1>');
 }).listen(80);

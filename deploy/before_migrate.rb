c7d9e2dfe35c99f0383302867ac80edff56f1f45
Daniel Hüsch <daniel.huesch@gmail.com>
2012-02-28 07:12:43 +0100

    wip

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,6 +1,6 @@
 ::File.open('/tmp/dhdbg', 'a') do |dhdbg| 
   dhdbg.puts "::File.expand_path(__FILE__): #{::File.expand_path(__FILE__)}"
   dhdbg.puts "release_path: #{release_path}"
-  dhdbg.puts "run_context.send(:release_path): #{run_context.send(:release_path)}"
+  #dhdbg.puts "run_context.send(:release_path): #{run_context.send(:release_path)}"
 end
 

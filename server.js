f8a94d6217c475c0895b6ab89ac9c1e5d4e62de1
Daniel Hüsch <daniel.huesch@gmail.com>
2012-02-28 06:47:35 +0100

    wip

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  fs.readFile('/tmp/dhdbg' function (err, data) {
+  fs.readFile('/tmp/dhdbg', function (err, data) {
     res.end('<h1>This is node.js ' + process.version + '</h1><pre>' + data + '</pre>');
   });
 }).listen(80);

daf43ea8aef7d6ee1476c400817f68caccc4e143
Daniel Hüsch <daniel.huesch@gmail.com>
2012-02-28 06:31:46 +0100

    wip

---
+++ deploy/before_migrate.rb
@@ -0,0 +1,2 @@
+puts "File.expand_path(__FILE__): #{File.expand_path(__FILE__)}"
+
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>This is node.js ' + process.version + '</h1>');
+  fs.readFile('/tmp/dhdbg' function (err, data) {
+    res.end('<h1>This is node.js ' + process.version + '</h1><pre>' + data + '</pre>');
+  });
 }).listen(80);

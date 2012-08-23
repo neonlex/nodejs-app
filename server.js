b07a3e8e77af29209e4b73c37737d281b740283f
Daniel Spangenberg <danielsp@amazon.com>
2012-08-23 21:26:08 +0200

    add hostname to output

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+var exec = require('child_process').exec,
+    child;
 var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>This is node.js ' + process.version + '</h1>');
+  exec('hostname', function(error, stdout, stderr) {
+    res.end('<h1>This is node.js ' + process.version + '</h1>' + '</br>' + stdout);
+  });
 }).listen(80);
